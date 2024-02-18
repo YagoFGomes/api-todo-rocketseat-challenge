846d09d6a8c1284051d02487a172b42494259b32
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-18 17:15:02 +0000

    implement a creation task route

---
+++ src/http/todo.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
+import { z } from 'zod';
+
+export async function toDoRoutes(app: FastifyInstance){
+    app.post('/tasks', async (request, reply) => {
+        // Deve ser possível criar uma task no banco de dados, enviando os campos `title` e `description` por meio do `body` da requisição.
+        // Ao criar uma task, os campos: `id`, `created_at`, `updated_at` e `completed_at` devem ser preenchidos automaticamente, conforme a orientação das propriedades acima.
+        
+        const createTaskBodySchema = z.object({
+            title: z.string(),
+            description: z.string(),
+        });
+
+        const {title, description} = createTaskBodySchema.parse(request.body);
+
+        await prisma.task.create({
+            data: {
+                title,
+                description,      
+            }
+        });
+
+        return reply.status(201).send();
+    });
+
+    app.get('/tasks', (_, reply)=>{
+        // Deve ser possível listar todas as tasks salvas no banco de dados.
+
+        // Também deve ser possível realizar uma busca, filtrando as tasks pelo `title` e `description`
+        return reply.status(201).send();
+    });
+
+
+    app.put('/tasks/:id', ()=>{
+        // Deve ser possível atualizar uma task pelo `id`.
+
+        // No `body` da requisição, deve receber somente o `title` e/ou `description` para serem atualizados.
+
+        // Se for enviado somente o `title`, significa que o `description` não pode ser atualizado e vice-versa.
+
+        // Antes de realizar a atualização, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
+    });
+
+
+    app.delete('/tasks/:id', ()=>{
+        // Deve ser possível remover uma task pelo `id`.
+
+        // Antes de realizar a remoção, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
+    });
+
+    app.delete('/tasks/:id/complete', ()=>{
+        // Deve ser possível marcar a task como completa ou não. Isso significa que se a task estiver concluída, deve voltar ao seu estado “normal”.
+
+        // Antes da alteração, deve ser feito uma validação se o `id` pertence a uma task salva no banco de dados.
+    });
+}
